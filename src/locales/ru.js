10b8444ad5ddc6142a7312e705504d03f1e15c8d
Larisa <isaykinalarisa@gmail.com>
2022-12-13 11:38:43 -0500

    created .eslintignore

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,21 +1,19 @@
-
 export default {
-    translation: {
-      header1: 'RSS агрегатор',
-      header2: 'Начните читать RSS сегодня! Это легко, это красиво.',
-      urlExample: 'Пример: https://ru.hexlet.io/lessons.rss',
-      btnSubmit: 'Добавить',
-      inputLabel: 'Ссылка RSS',
-      successMessage: 'RSS успешно загружен',
-      existingRssError: 'RSS уже существует',
-      invalidRssError: 'Ссылка должна быть валидным URL',
-      emptyRss: 'Не должно быть пустым',
-      noRssError: 'Ресурс не содержит валидный RSS',
-      networkError: 'Ошибка сети',
-      read: 'Читать полностью',
-      close: 'Закрыть',
-      see: 'Просмотр',
-      
+  translation: {
+    header1: 'RSS агрегатор',
+    header2: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    urlExample: 'Пример: https://ru.hexlet.io/lessons.rss',
+    btnSubmit: 'Добавить',
+    inputLabel: 'Ссылка RSS',
+    successMessage: 'RSS успешно загружен',
+    existingRssError: 'RSS уже существует',
+    invalidRssError: 'Ссылка должна быть валидным URL',
+    emptyRss: 'Не должно быть пустым',
+    noRssError: 'Ресурс не содержит валидный RSS',
+    networkError: 'Ошибка сети',
+    read: 'Читать полностью',
+    close: 'Закрыть',
+    see: 'Просмотр',
 
-    },
-  };
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,50 @@
-
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-
 export default {
-  mode:  process.env.NODE_ENV || 'development',
+  mode: process.env.NODE_ENV || 'development',
   devtool: false,
   devServer: {
     open: true,
   },
   entry: './src/index.js',
   output: {
-      filename: 'main.js',
-      path: resolve(__dirname,  'dist')
- },
+    filename: 'main.js',
+    path: resolve(__dirname, 'dist'),
+  },
 
   module: {
     rules: [
-   
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
+
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
           },
         },
-        { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] },
-        {
-          test: /\.scss$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
-        },
-        {
-          test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          use: 'url-loader?limit=10000',
-        },
-        {
-          test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-          use: 'file-loader',
-        },
-    
+      },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+
     ],
   },
   plugins: [
@@ -55,7 +53,7 @@ export default {
       title: 'Development',
       template: 'index.html',
     }),
-   
+
   ],
 };
-////////////////////////////////////////////////////////////////
+/// /////////////////////////////////////////////////////////////
